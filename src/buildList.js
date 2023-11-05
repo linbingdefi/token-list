80558133b2b5e006e8a0ee82a78df805be5e7205
linbing <134373343+linbingdefi@users.noreply.github.com>
2023-11-05 12:37:47 +0300

    Add files via upload

---
+++ src/buildList.js
@@ -0,0 +1,34 @@
+const { version } = require("../package.json");
+// const mainnet = require("./tokens/mainnet.json");
+// const ropsten = require("./tokens/ropsten.json");
+// const rinkeby = require("./tokens/rinkeby.json");
+// const goerli = require("./tokens/goerli.json");
+// const kovan = require("./tokens/kovan.json");
+// const bscMainnet = require("./tokens/bsc-mainnet.json");
+// const bscTestnet = require("./tokens/bsc-testnet.json");
+const arbitrumGoerli = require("./tokens/arbitrum-mainet.json");
+const arbitrumMainet = require("./tokens/arbitrum-goerli.json");
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  return {
+    name: "Linbing Default",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI: "https://raw.githubusercontent.com/linbing-tokens/default-token-list/main/linbing.jpg",
+    keywords: ["linbing", "default"],
+    tokens: [...arbitrumGoerli, ...arbitrumMainet]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      }),
+  };
+};
